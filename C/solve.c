181a7a0db624c63941cbc7378ca5ec407cb4c655
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-09-03 09:34:58 +0000

    [Add] khe - Add Files

---
+++ C/solve.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define INF 0x3f3f3f3f;
+#define NOP ;
+#define MAXV 100000
+
+int n;
+
+int main() {
+    
+	scanf("%d", &n);
+    printf("%d\n", n);
+
+	return 0;
+}
---
+++ CPP/solve.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+#include <cstring>
+
+using namespace std;
+constexpr int INF = 0x3f3f3f3f;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define NOP ;
+#define MAXN 100000
+
+int n;
+vector<int> vec;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	cout << n;
+
+	return 0;
+}
