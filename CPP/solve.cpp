2be262cc3d0bd6b43d9d2d2aa3d8f795e61028d2
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-11-16 11:56:02 +0000

    [Try] khe - Try 16496

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,6 +1,6 @@
 /**************************************************************
 * SOLVED	:	1
-* CURRENT	: 	10769
+* CURRENT	: 	16496
 * NEXT 		: 	15720
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -18,38 +18,31 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-string str;
-int cmp;
-bool flag = false;
+int n;
+long long result;
+string input[101];
+
+int cmp(string a, string b) {
+	return a + b > b + a;
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> str;
-	for(int i=0; i < str.size(); i++) {
-		if(str.substr(i, 3) == ":-)") {
-			if(!flag) flag = true;
-			cmp++;
-			i += 2;
-		}
-		else if(str.substr(i, 3) == ":-(") {
-			if(!flag) flag = true;
-			cmp--;
-			i += 2;
-		}
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cin >> input[i];
 	}
 
-	if(flag) {
-		if(cmp > 0) cout << "happy";
-		else if(cmp < 0) cout << "sad";
-		else if(cmp == 0) cout << "unsure";
-	}
-	else {
-		cout << "none";
+	sort(input, input + n, cmp);
+
+	string s_res = "";
+	for (int j = 0; j < n; j++) {
+		s_res += input[j];
 	}
 
-	cout << "\n";
+	cout << s_res << endl;
 
 	return 0;
 }
