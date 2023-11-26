ee96faadab10494e615995a6e277c418a172de2f
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-11-26 03:59:18 +0000

    [Edit] khe - implement DFS

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -17,20 +17,58 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-int n;
-int cnt = 2;
+int n, k, m;
+int x, y, g;
+bool isy, isp;
+
+int dig(int a) {
+	if (a >= 0 && a <= 9) {
+		return a;
+	}
+	
+	int sum = 0;
+	while(a > 0) {
+		sum += x%10;
+	}
+	return dig(sum);
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
 	cin >> n;
-
 	for(int i=0; i<n; i++) {
-		cnt += cnt-1;
+		cin >> k >> m;
+		
+		x = y = 0; g = 1;
+		isy = isp = true;
+		for(int j=1; j<=k; j++) {
+			int tmp = dig(g);
+			g *= m;
+			if(isy && isp) {
+				y += tmp;
+				isy = false;
+				isp = true;
+			}
+			else if(!isy && isp) {
+				x += tmp;
+				isy = true;
+				isp = false;
+			}
+			else if(isy && !isp) {
+				y -= tmp;
+				isy = false;
+				isp = false;
+			}
+			else if(!isy && !isp) {
+				x -= tmp;
+				isy = true;
+				isp = true;
+			}
+			cout << x << " " << y << endl;
+		}
 	}
 
-	cout << cnt*cnt << endl;
-	
 	return 0;
 }
--- CPP/tmp.cpp
+++ CPP/tmp.cpp
@@ -1,9 +1,13 @@
+/**************************************************************
+* dfs implementation
+***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <memory.h>
 #include <cstring>
 #include <string>
+#include <vector>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -13,58 +17,50 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-int n, k, m;
-int x, y, g;
-bool isy, isp;
+bool visited[9]; 			//방문 여부 저장
+vector<int> graph[9]; 		//vector 배열 생성(인접 노드에 대한 정보 저장)
 
-int dig(int a) {
-	if (a >= 0 && a <= 9) {
-		return a;
+int dfs(int x) {
+	visited[x] = true; 
+	cout << x << " ";
+	for(int i=0; i<graph[x].size(); i++) { //인접 노드 탐색
+		int y = graph[x][i];
+		if(!visited[y])	//방문하지 않으면, 해당 노드에 대한 탐색 시작
+			dfs(y); //재귀적 방문
 	}
-	
-	int sum = 0;
-	while(a > 0) {
-		sum += x%10;
-	}
-	return dig(sum);
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n;
-	for(int i=0; i<n; i++) {
-		cin >> k >> m;
-		
-		x = y = 0; g = 1;
-		isy = isp = true;
-		for(int j=1; j<=k; j++) {
-			int tmp = dig(g);
-			g *= m;
-			if(isy && isp) {
-				y += tmp;
-				isy = false;
-				isp = true;
-			}
-			else if(!isy && isp) {
-				x += tmp;
-				isy = true;
-				isp = false;
-			}
-			else if(isy && !isp) {
-				y -= tmp;
-				isy = false;
-				isp = false;
-			}
-			else if(!isy && !isp) {
-				x -= tmp;
-				isy = true;
-				isp = true;
-			}
-			cout << x << " " << y << endl;
-		}
-	}
+	graph[1].push_back(2);
+	graph[2].push_back(3);
+	graph[3].push_back(8);
 
+	graph[2].push_back(1);
+    graph[2].push_back(7);
+
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+
+    graph[6].push_back(7);
+
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+
+	dfs(1); //최초 노드 방문
+	
 	return 0;
 }
