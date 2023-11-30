1f9b4f0a6ba8db08af711ac88d5a9c563a19dda9
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-11-30 09:48:20 +0000

    [Solve] khe - Solve 2869

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	1012
+* CURRENT	: 	2869
 * NEXT 		: 	15720
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -19,53 +19,18 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-bool visited[51][51];
-bool a[51][51];
-
-int dx[4]{0, 0, 1, -1};
-int dy[4]{1, -1, 0, 0};
-
-int cnt = 0;
-int n, m, c, t;
-int x, y;
-
-
-void dfs(int y, int x) {
-	if(visited[y][x])
-		return;
-
-	for(int i=0; i<4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-			if(a[ny][nx]) {
-				dfs(ny, nx);
-			}
-		}
-	}
-}
+int a,b,v;
+int day = 0;
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> t;
-	for(int i=0; i<t; i++) {
-		cin >> n >> m >> c;
-		for(int j=0; j<c; j++) {
-			cin >> x >> y;
-			a[y][x] = 1;
-		}
-		for(int j=0; j<n; j++) {
-			for(int k=0; k<m; k++) {
-				if(a[j][k] && !visited[j][k]) {
-					dfs(j, k);
-					cnt++;
-				}
-			}
-		}
-		cout << cnt << endl;
-	}
+	cin >> a >> b >> v;
+	
+	day = (v - b - 1) / (a - b) + 1;
+
+	cout << day << endl;
+
 	return 0;
 }
