af445345257d82a1674963de3adf4788a96f8fcc
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-03 03:45:13 +0000

    [Solve] khe - Solve 2810

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	28279
+* CURRENT	: 	2810
 * NEXT 		: 	15720
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -18,141 +18,29 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-int n, tmp;
-int cmd;
-
-typedef struct Node {
-	Node *prev;
-	Node *next;
-	int data;
-};
-
-class CDeque {
-public:
-	Node *begin;
-	Node *end;
-	int size = 0;
-};
+int n;
+char c;
+bool flag;
+double sum;
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	CDeque a;
-
 	cin >> n;
-	for(int i=0; i<n; i++) {
-		cin >> cmd;
-		if(cmd == 1) {
-			cin >> tmp;
-			if(a.size == 0) {
-				Node *nw = new Node;
-				a.begin = nw; a.end = nw;
-				a.begin->data = tmp;
-				
-				a.begin->next = 0;
-				a.begin->prev = 0;
-				a.size++;
-			}
-			else {
-				Node *nw = new Node;
-				nw->data = tmp;
-				
-				nw->next = a.begin;
-				nw->prev = 0;
-				
-				a.begin->prev = nw;
-				a.begin = nw;
-				a.size++;
-			}
-		}
-		else if(cmd == 2) {
-			cin >> tmp;
-			if(a.size == 0) {
-				Node *nw = new Node;
-				a.begin = nw; a.end = nw;
-				a.begin->data = tmp;
-				
-				a.begin->next = 0;
-				a.begin->prev = 0;
-				a.size++;
-			}
-			else {
-				Node *nw = new Node;
-				nw->data = tmp;
 
-				a.end->next = nw;
-				nw->prev = a.end;
-				a.end = nw;
-				a.size++;
-			}
-		}
-		else if(cmd == 3) {
-			if(a.size == 0) {
-				cout << -1 << endl;
-			}
-			else if(a.size == 1) {
-				cout << a.begin->data << endl;
-				delete a.begin;
-				a.begin = 0; a.end = 0;
-				a.size--;
-			}
-			else {
-				cout << a.begin->data << endl;
-				Node *nw = a.begin->next;
-				delete a.begin;
-				a.begin = nw;
-				a.begin->prev = 0;
-				a.size--;
-			}
-		}
-		else if(cmd == 4) {
-			if(a.size == 0) {
-				cout << -1 << endl;
-			} 
-			else if(a.size == 1) {
-				cout << a.end->data << endl;
-				delete a.end;
-				a.begin = 0; a.end = 0;
-				a.size--;
-			}
-			else {
-				cout << a.end->data << endl;
-				Node *nw = a.end->prev;
-				delete a.end;
-				a.end = nw;
-				a.end->next = 0;
-				a.size--;
-			}
-		}
-		else if(cmd == 5) {
-			cout << a.size << endl;
-		}
-		else if(cmd == 6) {
-			if(a.size > 0)
-				cout << 0 << endl;
-			else
-				cout << 1 << endl;
-		}
-		else if(cmd == 7) {
-			if(a.size <= 0) {
-				cout << -1 << endl;
-			}
-			else {
-				cout << a.begin->data << endl;
-			}
-
-		}
-		else if(cmd == 8) {
-			if(a.size <= 0) {
-				cout << -1 << endl;
-			}
-			else {
-				cout << a.end->data << endl;
-			}
-		}
-	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> c;
+		if (c == 'S') sum++;
+		else //(c == 'L')
+		{
+			flag = true;
+			sum += 0.5;
+		}	
 	}
-
+	
+	if (flag) cout << sum + 1;
+	else cout << sum;
 	return 0;
 }
