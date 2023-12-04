5eab769b307e9257c88d1a66587eb0e25329d06f
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-04 10:15:02 +0000

    [Solve] khe - Solve 2468(failed)"

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	2810
+* CURRENT	: 	2468
 * NEXT 		: 	15720
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -18,10 +18,35 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100000
 
-int n;
-char c;
-bool flag;
-double sum;
+int a[101][101];
+bool visited[101][101];
+int n, cnt, minh, maxh;
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0,  0, 1, -1};
+vector<int> tmp;
+int res = 0;
+
+void dfs(int y, int x, int h) {
+	for(int i=0; i<n; i++) {
+		int nx = x + dx[i];
+		int ny = x + dy[i];
+
+		if(nx >= 0 && ny >= 0 && nx < n && ny < n) {
+			if(!visited[ny][nx] && a[ny][nx] > h) {
+				visited[ny][nx] = true;
+				dfs(ny, nx, h);
+			}
+		}
+	}
+}
+
+void init() {
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			visited[i][j] = false;
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -29,18 +54,37 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> c;
-		if (c == 'S') sum++;
-		else //(c == 'L')
-		{
-			flag = true;
-			sum += 0.5;
-		}	
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			cin >> a[i][j];
+			tmp.push_back(a[i][j]);
+
+		}
+	}
+
+	int minh = *min_element(tmp.begin(), tmp.end());	
+	int maxh = *max_element(tmp.begin(), tmp.end());
+
+	for(int h=minh; h<maxh; h++) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(!visited[i][j]) {
+					visited[i][j] = true;
+					dfs(j, i, h);
+					cnt++;
+				}
+			}
+		}
+		if(cnt == 0)
+			break;
+		if(res < cnt)
+			res = cnt;
+		cnt = 0;
+		init();
 	}
 	
-	if (flag) cout << sum + 1;
-	else cout << sum;
+	
+	cout << res << endl;
+
 	return 0;
 }
