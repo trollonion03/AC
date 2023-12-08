85d7daaa16246276722bb1749d0e0583bc8e90d1
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-08 12:01:10 +0000

    [Edit] khe - try 21736

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	11724
+* CURRENT	: 	21736
 * NEXT 		: 	15720
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -17,28 +17,24 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define NOP ;
-#define MAXN 1001
+#define MAXN 601
 
-int n, m, u, v;
+int n, m;
 int cnt = 0;
-vector<int> a[MAXN];
-bool visited[MAXN];
-queue<int> q;
+string a[MAXN][MAXN];
+bool visited[MAXN][MAXN];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
-void bfs(int x) {
-	q.push(x);
-	visited[x] = true;
-
-	while(!q.empty()) {
-		int tmp = q.front();
-		q.pop();
-		for(int i=0; i<a[tmp].size(); i++) {
-			int nxt = a[tmp][i];
-			if(!visited[nxt]) {
-				q.push(nxt);
-				visited[nxt] = true;
-			}
+void dfs(int x, int y) {
+	
+	for(int i=0; i<4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if(nx >= 1 && ny >= 1 && nx <= n && ny <= y) {
+			if(v)
 		}
+		
 	}
 }
  
@@ -47,16 +43,15 @@ int main() {
 	cin.tie(0); cout.tie(0);
 
 	cin >> n >> m; 
-	for(int i=0; i<m; i++) {
-		cin >> u >> v;
-		a[u].push_back(v);
-		a[v].push_back(u);
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=m; j++) {
+			cin >> a[n][m];
+		}
 	}
 
 	for(int i=1; i<=n; i++) {
-		if(!visited[i]) {
-			bfs(i);
-			cnt++;
+		for(int j=1; j<=m; j++) {
+			dfs(i, j);
 		}
 	}
 	cout << cnt << "\n";
