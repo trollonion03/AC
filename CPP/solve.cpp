37377d4b23cc4be4ff947d1c7602edb6a29f85e8
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-13 11:06:46 +0000

    [Solve] khe - Solve 7662

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	11123
+* CURRENT	: 	7662
 * NEXT 		: 	7569
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <set>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -19,70 +20,52 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 101
 
-int t, n, m;
-int cnt = 0;
-int a[MAXN][MAXN];
-bool visited[MAXN][MAXN];
-queue<pair<int, int>> q;
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-
-void bfs(int x, int y) {
-	visited[x][y] = true;
-	q.push({x, y});
-
-	while(!q.empty()) {
-		int tx = q.front().first;
-		int ty = q.front().second;
-		q.pop();
-		for(int i=0; i<4; i++) {
-			int nx = tx + dx[i];
-			int ny = ty + dy[i];
-			if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-				if(!visited[nx][ny] && a[nx][ny]) {
-					visited[nx][ny] = true;
-					q.push({nx, ny});
-				}
-			}
-		}
-	}
-	
-}
- 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> t;
-	for(int i=0; i<t; i++) {
-		cin >> n >> m;
-		for(int j=0; j<n; j++) {
-			for(int k=0; k<m; k++) {
-				char tmp;
-				cin >> tmp;
-				if(tmp == '#') {
-					a[j][k] = 1;
-				}
-				else if (tmp == '.') {
-					a[j][k] = 0;
-				}
-			}
-		}
+	int t;
+    cin >> t;
 
-		for(int j=0; j<n; j++) {
-			for(int k=0; k<m; k++) {
-				if(!visited[j][k] && a[j][k]) {
-					visited[j][k] = true;
-					bfs(j, k);
-					cnt++;
-				}
-			}
-		}
-		cout << cnt << "\n";
-		cnt = 0;
-		memset(a, 0, sizeof(a));
-		memset(visited, 0, sizeof(visited));
-	}
+    int n;
+    while (t--)
+    {
+        cin >> n;
+        char order;
+        int num;
+        multiset<int> ms;
+        while (n--)
+        {
+            cin >> order >> num;
+            if (order == 'I')
+            {
+                ms.insert(num);
+            }
+            else
+            {
+                if (!ms.empty() && num == -1)
+                {
+                    ms.erase(ms.begin());
+                }
+                else if (!ms.empty() && num == 1)
+                {
+                    auto iter = ms.end();
+                    iter--;
+                    ms.erase(iter);
+                }
+            }
+        }
+        if (ms.empty())
+        {
+            cout << "EMPTY" << '\n';
+        }
+        else
+        {
+            auto end_iter = ms.end();
+            end_iter--;
+            cout << *end_iter << ' ' << *ms.begin() << '\n';
+        }
+    }
 	
 	return 0;
 }
