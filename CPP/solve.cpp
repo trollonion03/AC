3783461262d0fcdbeb339ff6b410ffba50cf520d
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-15 09:04:54 +0000

    [Solve] khe - Solve 1303

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	7662
+* CURRENT	: 	1303
 * NEXT 		: 	7569
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -20,52 +20,69 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 101
 
+int n, m;
+string s;
+int a[MAXN][MAXN];
+bool visited[MAXN][MAXN];
+queue<pair<int, int>> q;
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+int wcnt = 0, bcnt = 0;
+int ctmp = 0;
+
+void bfs(int x, int y, int tgt) {
+    visited[x][y] = true;
+    q.push({x, y});
+    ctmp++;
+
+    while(!q.empty()) {
+        int xtmp = q.front().first;
+        int ytmp = q.front().second;
+        q.pop();
+        for(int i=0; i<4; i++) {
+            int nx = xtmp + dx[i];
+            int ny = ytmp + dy[i];
+            if(nx>=0 && ny>=0 && nx<m && ny<n && !visited[nx][ny] && a[nx][ny] == tgt) {
+                visited[nx][ny] = true;
+                q.push({nx, ny});
+                ctmp++;
+            }
+        }
+    }
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	int t;
-    cin >> t;
-
-    int n;
-    while (t--)
-    {
-        cin >> n;
-        char order;
-        int num;
-        multiset<int> ms;
-        while (n--)
-        {
-            cin >> order >> num;
-            if (order == 'I')
-            {
-                ms.insert(num);
+    cin >> n >> m;
+    for(int i=0; i<n; i++) {
+        cin >> s;
+        for(int j=0; j<m; j++) {
+            if(s[j] == 'W') {
+                a[i][j] = 1;
             }
-            else
-            {
-                if (!ms.empty() && num == -1)
-                {
-                    ms.erase(ms.begin());
-                }
-                else if (!ms.empty() && num == 1)
-                {
-                    auto iter = ms.end();
-                    iter--;
-                    ms.erase(iter);
-                }
+            else if(s[j] == 'B') {
+                a[i][j] = 0;
             }
-        }
-        if (ms.empty())
-        {
-            cout << "EMPTY" << '\n';
-        }
-        else
-        {
-            auto end_iter = ms.end();
-            end_iter--;
-            cout << *end_iter << ' ' << *ms.begin() << '\n';
         }
     }
-	
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if(!visited[i][j]) {
+                bfs(i, j, a[i][j]);
+                if(a[i][j]) {
+                    wcnt += ctmp * ctmp;
+                }
+                else {
+                    bcnt += ctmp * ctmp;
+                }
+                ctmp = 0;
+            }
+        }
+    }
+    
+    cout << wcnt << " " << bcnt << "\n";	
 	return 0;
 }
