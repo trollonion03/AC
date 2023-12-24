42b9fe5a4be32d7cf6c0d4ee9adb00aeedfd93cc
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-24 03:03:48 +0000

    [Solve] khe - Solve 1697

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	16928
+* CURRENT	: 	1697
 * NEXT 		: 	7569
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -17,48 +17,45 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define NOP ;
-#define MAXN 101
+#define MAXN 100001
 
-int n, m, s, e;
-int lds[MAXN];
-queue<pair<int,int>> q;
+int n, m;
+int ans = INF;
+queue<pair<int, int>> q;
 int visited[MAXN];
 
 void bfs() {
-	int ans = INF;
-	q.push({1, 0});
-	visited[1] = true;
+	q.push({n, 0});
+	visited[n] = false;
 
 	while(!q.empty()) {
 		int cur = q.front().first;
 		int cnt = q.front().second;
 		q.pop();
 
-		if(cur == 100) {
+		if(cur == m) {
 			ans = min(ans, cnt);
-			break;
 		}
 
-		for(int i=1; i<=6; i++) {
-			int next = cur + i;
+		int neg = cur - 1;
+		if(neg < MAXN && neg >= 0 && !visited[neg]) {
+			q.push({neg, cnt+1});
+			visited[neg] = true;
+		}
 
-			if(next > 100 || visited[next]) {
-				continue;
-			}
+		int pos = cur + 1;
+		if(pos < MAXN && pos >= 0 && !visited[pos]) {
+			q.push({pos, cnt+1});
+			visited[pos] = true;
+		}
 
-			visited[next] = true;
-			
-			if(lds[next] == 0) {
-				q.push({next, cnt+1});
-				
-			}
-			else {
-				q.push({lds[next], cnt+1});
-			}
+		int dlx = cur * 2;
+		if(dlx < MAXN && dlx >= 0 && !visited[dlx]) {
+			q.push({dlx, cnt+1});
+			visited[dlx] = true;
 		}
 	}
-	cout << ans << "\n";
-	return;
+	cout << ans << '\n';
 }
 
 int main() {
@@ -66,12 +63,6 @@ int main() {
 	cin.tie(0); cout.tie(0);
 
 	cin >> n >> m;
-	memset(lds, 0, sizeof(lds));
-	for(int i=0; i<n; i++) {
-		cin >> s >> e;
-		lds[s] = e;
-	}
-	
 	bfs();
 
 	return 0;
