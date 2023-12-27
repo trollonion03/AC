90bf1c0f37046df046ab99d0dd2ac089b6a6b4fa
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-27 04:18:08 +0000

    [Solve] khe - Solve 1167

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	2206
+* CURRENT	: 	1167
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,7 +10,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include <queue>
+#include <stack>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -18,81 +18,67 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define NOP ;
-#define MAXN 101
+#define MAXN 100001
 
-typedef struct st {
+typedef struct tp {
 	int x;
-	int y;
-	int cnt;
-	bool flag;
+	int dist;
 };
 
-int n, m;
-int ans = INF;
-string input;
-int a[MAXN][MAXN];
-bool visited[MAXN][MAXN];
-queue<st> q;
+int n, na, nb, dst, mxn;
+int ans = -1;
+bool visited[MAXN];
+stack<tp> st;
+vector<pair<int,int>> a[MAXN];
 
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int dfs(int start) {
+	int an = -1;
+	st.push({start, 0});
+	visited[start] = true;
 
-void bfs() {
-	q.push({0, 0, 1, 0});
-	visited[0][0] = true;
-
-	while(!q.empty()) {
-		int x = q.front().x;
-		int y = q.front().y;
-		int cnt = q.front().cnt;
-		bool flag = q.front().flag;
-		q.pop();
-
-		if(x == n-1 && y == m-1) {
-			ans = min(ans, cnt);
+	while(!st.empty()) {
+		int cur = st.top().x;
+		int dist = st.top().dist;
+		st.pop();
+		
+		if(an < dist) {
+			an = dist;
+			mxn = cur;
 		}
 
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+		for(int i=0; i<a[cur].size(); i++) {
+			int next = a[cur][i].first;
+			int ndst = a[cur][i].second;
 
-			if(nx>=0 && ny>=0 && x<n && y<m && !visited[nx][ny]) {
-				if(a[nx][ny] == 0){
-					q.push({nx, ny, cnt+1, flag});
-					visited[nx][ny] = true;
-				}
-
-				if(a[nx][ny] == 1 && !flag) {
-					q.push({nx, ny, cnt+1, true});
-					visited[nx][ny] = true;
-					a[nx][ny] = 0;
-				}
+			if(!visited[next]) {
+				st.push({next, dist+ndst});
+				visited[next] = true;
 			}
 		}
 	}
-
-	if(ans != INF) {
-		cout << ans << "\n";
-	}
-	else {
-		cout << -1 << "\n";
-	}
-	
+	return an;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n >> m;
+	cin >> n;
 	for(int i=0; i<n; i++) {
-		cin >> input;
-		for(int j=0; j<input.size(); j++) {
-			a[i][j] = (int)input[j] - (int)'0';
+		cin >> na;
+		while(1) {
+			cin >> nb;
+			if(nb == -1) break;
+			cin >> dst;
+			a[na].push_back({nb, dst});
+			a[nb].push_back({na, dst});
 		}
 	}
 
-	bfs();
-
+	dfs(1);
+	memset(visited, 0, sizeof(visited));
+	ans = dfs(mxn);
+	cout << ans << "\n";
+	
 	return 0;
 }
