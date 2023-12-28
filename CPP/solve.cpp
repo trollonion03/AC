55d3af0ef4fce52c3b746fe6be3f180db45fd7fc
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-28 03:40:05 +0000

    [Solve] khe - Solve 1967

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	1158
+* CURRENT	: 	1967
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,7 +10,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include <queue>
+#include <stack>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -20,30 +20,62 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 100001
 
-int n, k;
-queue<int> q;
+typedef struct tp {
+	int x;
+	int dist;
+};
+
+int n, na, nb, dst, mxn;
+int ans = -1;
+bool visited[MAXN];
+stack<tp> st;
+vector<pair<int,int>> a[MAXN];
+
+int dfs(int start) {
+	int an = -1;
+	st.push({start, 0});
+	visited[start] = true;
+
+	while(!st.empty()) {
+		int cur = st.top().x;
+		int dist = st.top().dist;
+		st.pop();
+		
+		if(an < dist) {
+			an = dist;
+			mxn = cur;
+		}
+
+		for(int i=0; i<a[cur].size(); i++) {
+			int next = a[cur][i].first;
+			int ndst = a[cur][i].second;
+
+			if(!visited[next]) {
+				st.push({next, dist+ndst});
+				visited[next] = true;
+			}
+		}
+	}
+	return an;
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n >> k;
-	for(int i=1; i<=n; i++) {
-		q.push(i);
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cin >> na;
+		cin >> nb;
+		cin >> dst;
+		a[na].push_back({nb, dst});
+		a[nb].push_back({na, dst});
 	}
 
-	cout << "<";
-	while (q.size()>1) {
-		for(int i=0; i<k-1;i++) {
-			int tmp = q.front();
-			q.pop();
-			q.push(tmp);
-		}
-		int ans = q.front();
-		q.pop();
-		cout << ans << ", ";
-	}
-	cout << q.front() << ">\n";
+	dfs(1);
+	memset(visited, 0, sizeof(visited));
+	ans = dfs(mxn);
+	cout << ans << "\n";
 	
 	return 0;
 }
