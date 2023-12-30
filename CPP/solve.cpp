1043c6aa7b80d6af60b86b49434b3c683b24afb3
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-30 09:41:31 +0000

    [Solve] khe - Solve 2578

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	2845
+* CURRENT	: 	2578
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -18,20 +18,102 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define NOP ;
-#define MAXN 100001
+#define MAXN 26
 
-int l, p, tmp;
+int input;
+int cnt = 0;
+int a[MAXN][MAXN];
+bool visited[MAXN][MAXN];
+vector<pair<int,int>> vec[MAXN];
+bool xc[6];
+bool yc[6];
+bool d1f = false, d2f = false;
+int bcnt=0;
+
+bool check(int x) {
+	int posx = vec[x][0].first;
+	int posy = vec[x][0].second;
+
+	if(visited[posx][posy]) {
+		return false;
+	}
+
+	visited[posx][posy] = true;
+	
+	int xcnt = 0;
+	int ycnt = 0;
+	int d1 = 0;
+	int d2 = 0;
+	for(int i=0; i<5; i++) {
+		if(!xc[posx]) {	
+			if(visited[posx][i]) {
+				xcnt++;
+				if(xcnt == 5) {
+					bcnt++;
+					xc[posx] = true;
+					if(bcnt == 3)
+						return true;
+				}
+			}
+		}
+		if(!yc[posy]) {
+			if(visited[i][posy]) {
+				ycnt++;
+				if(ycnt == 5) {
+					bcnt++;
+					yc[posy] = true;
+					if(bcnt == 3)
+						return true;
+				}
+			}
+		}
+		if(!d1f) {
+			if(visited[i][i]) {
+				d1++;
+				if(d1 == 5) {
+					bcnt++;
+					d1f = true;
+					if(bcnt == 3)
+						return true;
+				}
+			}
+		}
+		if(!d2f) {
+			if(visited[i][4-i]) {
+				d2++;
+				if(d2 == 5) {
+					bcnt++;
+					d2f = true;
+					if(bcnt == 3)
+						return true;
+				}
+			}
+		}	
+	}
+	return false;
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> l >> p;
 	for(int i=0; i<5; i++) {
-		cin >> tmp;
-		cout << tmp - l * p << " ";
+		for(int j=0; j<5; j++) {
+			cin >> a[i][j];
+			vec[a[i][j]].push_back({i, j});
+		}
 	}
-	cout << endl;
+
+	for(int i=0; i<5; i++) {
+		for(int j=0; j<5; j++) {
+			cin >> input;
+			bool isBingo = check(input);
+			cnt++;
+			if(isBingo) {
+				cout << cnt << "\n";
+			}
+		}
+	}
 	
 	return 0;
 }
