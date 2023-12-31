75c1a357b48066045320a8d2347ce73a53dd98a0
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2023-12-31 04:06:23 +0000

    [Solve] khe - Solve 2638

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	2578
+* CURRENT	: 	2638
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,7 +10,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include <stack>
+#include <queue>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -18,102 +18,83 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 #define NOP ;
-#define MAXN 26
+#define MAXN 101
 
-int input;
+int n, m;
 int cnt = 0;
 int a[MAXN][MAXN];
+queue<pair<int, int>> q;
+vector<pair<int, int>> v;
 bool visited[MAXN][MAXN];
-vector<pair<int,int>> vec[MAXN];
-bool xc[6];
-bool yc[6];
-bool d1f = false, d2f = false;
-int bcnt=0;
+int outside[MAXN][MAXN];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
-bool check(int x) {
-	int posx = vec[x][0].first;
-	int posy = vec[x][0].second;
+void bfsAir() {
+	q.push({0, 0});
+	visited[0][0] = true;
 
-	if(visited[posx][posy]) {
-		return false;
+	while(!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny]) {
+				if(a[nx][ny] == 1) {
+					outside[nx][ny]++;
+				}
+				else {
+					q.push({nx, ny});
+					visited[nx][ny] = true;
+					if (a[nx][ny] == 0) {
+						a[nx][ny] = 5;
+					}
+				}
+			}
+		}
 	}
-
-	visited[posx][posy] = true;
-	
-	int xcnt = 0;
-	int ycnt = 0;
-	int d1 = 0;
-	int d2 = 0;
-	for(int i=0; i<5; i++) {
-		if(!xc[posx]) {	
-			if(visited[posx][i]) {
-				xcnt++;
-				if(xcnt == 5) {
-					bcnt++;
-					xc[posx] = true;
-					if(bcnt == 3)
-						return true;
-				}
-			}
-		}
-		if(!yc[posy]) {
-			if(visited[i][posy]) {
-				ycnt++;
-				if(ycnt == 5) {
-					bcnt++;
-					yc[posy] = true;
-					if(bcnt == 3)
-						return true;
-				}
-			}
-		}
-		if(!d1f) {
-			if(visited[i][i]) {
-				d1++;
-				if(d1 == 5) {
-					bcnt++;
-					d1f = true;
-					if(bcnt == 3)
-						return true;
-				}
-			}
-		}
-		if(!d2f) {
-			if(visited[i][4-i]) {
-				d2++;
-				if(d2 == 5) {
-					bcnt++;
-					d2f = true;
-					if(bcnt == 3)
-						return true;
-				}
-			}
-		}	
-	}
-	return false;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	for(int i=0; i<5; i++) {
-		for(int j=0; j<5; j++) {
+	cin >> n >> m;
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<m; j++) {
 			cin >> a[i][j];
-			vec[a[i][j]].push_back({i, j});
 		}
 	}
 
-	for(int i=0; i<5; i++) {
-		for(int j=0; j<5; j++) {
-			cin >> input;
-			bool isBingo = check(input);
-			cnt++;
-			if(isBingo) {
-				cout << cnt << "\n";
-			}
+	bool flag = false;
+	for(;;) {
+		bfsAir();
+		for(int i=1; i<n-1; i++) {
+			for(int j=1; j<m-1; j++) {
+				if(outside[i][j] >= 2) {
+					flag = true;
+					a[i][j] = 0;
+				}
+			} 
 		}
-	}
-	
+
+		if(flag) {
+			cnt++;
+		}
+		else {
+			break;
+		}
+
+		memset(visited, 0, sizeof(visited));
+		memset(outside, 0, sizeof(outside));
+		flag = false;
+	}	
+
+	cout << cnt << "\n";
+
 	return 0;
 }
