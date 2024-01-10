484e07a9a4405df54bbed31607d757a4972ad6c3
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-01-10 07:14:32 +0000

    [Solve] khe - Solve 3025

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -10,7 +10,8 @@
 #include <cstdlib>
 #include <cstdio>
 #include <string>
-#include <vector>
+#include <stack>
+#include <set>
 #include <deque>
 
 using namespace std;
@@ -22,10 +23,60 @@ typedef unsigned long long ull;
 #define NOP ;
 #define MAXN 30001
 
+typedef struct f {
+	int start;
+	int y; 
+	int x;
+};
+
 int r, c, n, s;
 char a[MAXN][31];
-vector<pair<int, int>> wall[31];
-deque<pair<int, int>> q;
+set<int> wall[31];
+stack<pair<int, int>> q[31];
+
+// 시작 열, y좌표, x좌표
+void roll(int sp, int sy, int sx) {
+	//재귀 함수로 구현시 디버깅이 어려움으로, stack를 사용하여 구현
+	stack<f> stk;
+	stk.push({sp, sy, sx});
+	
+	while(!stk.empty()) {
+		int x = stk.top().x;
+		int y = stk.top().y;
+		int start = stk.top().start;
+		int w = *(wall[x].upper_bound(y));
+		stk.pop();
+
+		//벽 위에 떨어진 경우
+		if(a[w][x] == 'X') {
+			a[w-1][x] = 'O';
+			wall[x].insert(w-1);
+			wall[x].erase(w);
+			continue;
+		}
+		
+		//돌 위에 떨어진 경우
+		if(a[w][x] == 'O') {
+			if(x-1>=0 && a[w-1][x-1] == '.' && a[w][x-1] == '.') {
+				q[start].push({w-1, x});
+				stk.push({start, w-1, x-1});
+				continue;
+			}
+			if(x+1<c && a[w-1][x+1] == '.' && a[w][x+1] == '.') {
+				q[start].push({w-1, x});
+				stk.push({start, w-1, x+1});
+				continue;
+			}
+			else {
+				a[w-1][x] = 'O';
+				wall[x].erase(w);
+				wall[x].insert(w-1);
+				continue;
+			}
+		}
+	}
+	
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -37,79 +88,39 @@ int main() {
 			cin >> a[i][j];
 			if(a[i][j] == 'X') {
 				//벽 정보 저장 (j열에 i번째줄)
-				wall[j].push_back({i, 1});
-				//1: 벽, 2: 돌
+				wall[j].insert(i);
 			}
 		}
+	}
+
+	for(int i=0; i<c; i++) {
+		wall[i].insert(r);
+		a[r][i] = 'X';
 	}
 
 	cin >> n;
+	int cx, cy;
 	for(int i=0; i<n; i++) {
 		cin >> s;
-		q.push_back({s-1, 0});
-	}
-
-	while(!q.empty()) {
-		int st = q.front().first;
-		int h = q.front().second;
-		q.pop_front();
+		s--;
 		
+		while(!q[s].empty()) {
+			cy = q[s].top().first;
+			cx = q[s].top().second;
 
-		//조건 1: 해당 열에 벽이 없는 경우
-		if(wall[st].size() == 0) {
-			a[r-1][st] = 'O';
-			wall[st].push_back({r-1, 2});
-			continue;
-		}
-
-		//벽이 or 돌이 존재하는 경우
-		for(int i=0; i<wall[st].size(); i++) {
-			int pos = wall[st][i].first;
-			int check = wall[st][i].second;
-
-			//벽의 y 좌표가 돌 보다 높으면 skip
-			if(pos-1 < h) {
-				if(i == wall[st].size()-1) {
-					a[r-1][st] = 'O';
-					wall[st].push_back({r-1, 2});
-					break;
-				}
-				else {
-					continue;
-				}
-			}
-
-			// 조건 2: 아래가 벽일 경우
-			if(check == 1) {
-				a[pos-1][st] = 'O';
-				//insert로 벽 좌표 위에 돌덩이 위치 기록(주의 필요)
-				//vector로 중간 값 삽입 시 시간 복잡도가 O(N)이 된다.
-				//container + list 조합으로 해결해야할듯 하다.
-				wall[st].insert(wall[st].begin()+i, make_pair(pos-1, 2));
+			if(a[cy][cx] == '.') {
 				break;
 			}
-			//조건 3: 아래가 돌덩이인 경우
-			//queue->deque 이용
-			//TODO: 돌의 y좌표 속성 추가 & 검사 구현
-			if(check == 2) {
-				//3.1. 왼쪽 검사&왼쪽 아래 검사
-				if(st-1 >= 0 && a[pos-1][st-1] == '.' && a[pos][st-1] == '.') {
-					q.push_front({st-1, pos});
-					break;
-				}
+			q[s].pop();
 
-				//3.2. 오른쪽 검사&오른쪽 아래 검사
-				if(st+1 < c && a[pos-1][st+1] == '.' && a[pos][st+1] == '.') {
-					q.push_front({st+1, pos});
-					break;
-				}
-
-				//3.3. 둘 다 아닌 경우
-				a[pos-1][st] = 'O';
-				wall[st].insert(wall[st].begin()+i, make_pair(pos-1, 2));
-				break;
-			}
 		}
+		if(q[s].empty()) {
+			roll(s, 0, s);
+		}
+		else {
+			roll(s, cy, cx);
+		}
+		
 	}
 
 	for(int i=0; i<r; i++) {
@@ -123,3 +134,9 @@ int main() {
 }
 
 //반례는 다 맞는데 시간초과가 난다라,,,
+//솔루션 참고 - https://hillier.tistory.com/118 - 해당 솔루션의 로직을 일부 참고하였습니다.
+//일일이 돌이 떨어지는 위치를 탐색하게 된다면 벽이 매우 많은 경우 탐색 시간이
+//증가하기에 시간 초과가 발생하게 된다.
+//Stack을 이용해서 먼저 떨어지는 돌에 대한 체크포인트(바로가기)를 설정한 후
+//체크포인트에 장애물(벽 or 돌)이 존재한다면 해당 체크포인트를 제거하고
+//새로 탐색하는 방식으로 문제를 해결해야 시간 초과를 피할 수 있다.
