f4a904912ae9c0ffe72dcba0de6612ebb6330062
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-01-11 10:30:07 +0000

    [Solve] khe - Sovle 24479

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	9655
+* CURRENT	: 	24479
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -11,8 +11,7 @@
 #include <cstdio>
 #include <string>
 #include <stack>
-#include <set>
-#include <deque>
+#include <vector>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
@@ -21,18 +20,46 @@ typedef unsigned long long ull;
 
 #define pii pair<int,int>
 #define NOP ;
-#define MAXN 30001
+#define MAXN 100100
 
-int n;
+
+int n, m, s;
+int t1, t2;
+bool visited[MAXN];
+vector<int> a[MAXN];
+int result[MAXN] = { 0, };
+int cnt = 0;
+
+
+void dfs(int x) {
+	visited[x] = true;
+	cnt++;
+	result[x] = cnt;
+
+	for(int i=0; i<a[x].size(); i++) {
+		if(!visited[a[x][i]]) {
+			dfs(a[x][i]);
+		}
+	}
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	cin >> n;
-	if(n%2 == 1)
-		cout << "SK\n";
-	else
-		cout << "CY\n";
+	cin >> n >> m >> s;
+	for(int i=1; i<=m; i++) {
+		cin >> t1 >> t2;
+		a[t1].push_back(t2);
+		a[t2].push_back(t1);
+	}
 
-	return 0;
+	for(int i=1; i<=n; i++) {
+		sort(a[i].begin(), a[i].end());
+	}
+
+	dfs(s);
+	for(int i=1; i<=n; i++) {
+		cout << result[i] << "\n"; 
+	}
 }
