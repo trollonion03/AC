09bbf3bb9e5ae022543c8db7728fe1c738ccb2dc
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-01-17 10:31:51 +0000

    [Solve] khe - Solve 11779

--- CPP/timeSaving.cpp
+++ CPP/timeSaving.cpp
@@ -1,12 +1,13 @@
 /**************************************************************
 * Time-saving Solution (C&C++)
-* CURRENT	: 	1238
+* CURRENT	: 	11779
 * RETRY     :   0
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 #include <cstdio> // 시간초과 이슈로 printf(), scanf() 사용
 
 using namespace std;
@@ -16,11 +17,11 @@ typedef unsigned long long ull;
 
 #define MAXN 1010
 
-int n, m, v;
+int n, m, v1, v2;
 vector<pair<int, int>> a[MAXN];
+stack<int> ans;
 int d[MAXN];
-int ans[MAXN];
-int res = -1;
+int l[MAXN];
 
 void init() {
     for(int i=1; i<=n; i++) {
@@ -31,7 +32,6 @@ void init() {
 void dijkstra(int s) {
     priority_queue<pair<int, int>> pq;
     pq.push({0, s});
-    d[s] = 0;
     
     while(!pq.empty()) {
         int cur = pq.top().second;
@@ -49,32 +49,39 @@ void dijkstra(int s) {
             if(ndist < d[next]) {
                 d[next] = ndist;
                 pq.push({-ndist, next});
+                l[next] = cur;
             }
         }
     }
 }
 
 int main() {
-    scanf("%d %d %d", &n, &m, &v);
+    scanf("%d", &n);
+    scanf("%d", &m);
 
     for(int i=0; i<m; i++) {
         int aa, b, c;
         scanf("%d %d %d", &aa, &b, &c);
         a[aa].push_back({b, c});
     }
+    scanf("%d %d", &v1, &v2);
 
-    for(int i=1; i<=n; i++) {
-        init();
-        dijkstra(i);
-        ans[i] = d[v];
-    }
     init();
-    dijkstra(v);
-    for(int i=0; i<=n; i++) {
-        ans[i] += d[i];
-        res = max(res, ans[i]);
+    dijkstra(v1);
+    printf("%d\n", d[v2]);
+    while(true) {
+        ans.push(v2);
+        if(v2 == v1) {
+            break;
+        }
+        v2 = l[v2];
     }
-    printf("%d\n", res);
+    printf("%d\n", ans.size());
+    while(!ans.empty()) {
+        printf("%d ", ans.top());
+        ans.pop();
+    }
+    printf("\n");
 
     return 0;
 }
