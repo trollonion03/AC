3e2cfe4e9a8137d3928f0cd2b6110f5169773a90
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-02-02 09:41:49 +0000

    [Solve] khe - Solve 23793

--- CPP/timeSaving.cpp
+++ CPP/timeSaving.cpp
@@ -1,7 +1,7 @@
 /**************************************************************
 * Time-saving Solution (C&C++)
-* CURRENT	: 	18223
-* RETRY     :   1
+* CURRENT	: 	23793
+* RETRY     :   2
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -10,25 +10,25 @@
 #include <cstdio> // 시간초과 이슈로 printf(), scanf() 사용
 
 using namespace std;
-constexpr int INF = 987654321;
+constexpr int INF = 0x3f3f3f3f;
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define MAXN 5001
+#define MAXN 100001
 
-int v, e, p;
+int n, m, x, y, z;
 vector<pair<int, int>> a[MAXN];
 int d[MAXN];
-int ans[MAXN];
-int res = -1;
+int res1, res2;
 
 void init() {
-    for(int i=1; i<=v; i++) {
+    for(int i=1; i<=n; i++) {
         d[i] = INF;
     }
 }
 
-void dijkstra(int s) {
+void dijkstra(int s, int av) {
+    init();
     priority_queue<pair<int, int>> pq;
     pq.push({0, s});
     d[s] = 0;
@@ -46,7 +46,7 @@ void dijkstra(int s) {
             int next = a[cur][i].first;
             int ndist = dist + a[cur][i].second;
             
-            if(ndist < d[next]) {
+            if(ndist < d[next] && next != av) {
                 d[next] = ndist;
                 pq.push({-ndist, next});
             }
@@ -55,30 +55,39 @@ void dijkstra(int s) {
 }
 
 int main() {
-    scanf("%d %d %d", &v, &e, &p);
+    scanf("%d %d", &n, &m);
 
-    for(int i=0; i<e; i++) {
+    for(int i=0; i<m; i++) {
         int aa, b, c;
         scanf("%d %d %d", &aa, &b, &c);
         a[aa].push_back({b, c});
-        a[b].push_back({aa, c});
     }
 
-    init();
-    dijkstra(1);
+    scanf("%d %d %d", &x, &y, &z);
+
     
-    int t1 = d[p];
-    int t2 = d[v];
+    dijkstra(x, -1);
+    int t1 = d[y];
+    dijkstra(y, -1);
+    int t2 = d[z];
+    dijkstra(x, y);
+    int t3 = d[z];
 
-    dijkstra(p);
-    t1 += d[v];
 
-    if(t1 == t2) {
-        printf("SAVE HIM\n");
+    if(t1 == INF || t2 == INF) {
+        res1 = -1;
     }
     else {
-        printf("GOOD BYE\n");
+        res1 = t1+t2;
     }
+    if(t3 == INF) {
+        res2 = -1;
+    }
+    else {
+        res2 = t3;
+    }
+
+    printf("%d %d\n", res1, res2);
 
     return 0;
 }
