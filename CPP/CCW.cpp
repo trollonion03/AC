435694821e83295f79848e07e42c546837446891
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-02-13 09:35:26 +0000

    [Solve] khe - Solve 11758

--- CPP/CCW.cpp
+++ CPP/CCW.cpp
@@ -1,28 +1,26 @@
 /**************************************************************
-* CURRENT	: 	11758
-* NEXT 		: 	NULL
+* CCW implementation
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <memory.h>
 #include <cstring>
-#include <cstdlib>
-#include <cstdio>
 #include <string>
 #include <vector>
-#include <stack>
+#include <queue>
 
 using namespace std;
 constexpr int INF = 0x3f3f3f3f;
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define pii pair<int,int>
 #define NOP ;
-#define MAXN 100001
+#define MAXN 7
+#define pii pair<int, int>
+#define pip pair<int, pair<int, int>>
 
-int x1, x2, x3, y1, y2, y3;
+int x, y;
 
 typedef struct coord {
 	int x;
@@ -37,23 +35,10 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> x1 >> y1;
-	cin >> x2 >> y2;
-	cin >> x3 >> y3;
+	//양수: 시계 반대방향;
+	//음수: 시계방향;
+	//0:	평행
+	NOP;
 
-	int tmp = ccw({x1, y1}, {x2, y2}, {x3, y3});
-	if(tmp > 0) {
-		cout << 1 << "\n";
-		return 0;
-	}
-	if(tmp == 0) {
-		cout << 0 << "\n";
-		return 0;
-	}
-	if(tmp < 0) {
-		cout << -1 << "\n";
-		return 0;
-	}
-	
 	return 0;
 }
