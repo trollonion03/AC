baae7dc6ce31695ea810b48815ae1b40e2219d75
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-02-14 10:17:55 +0000

    [Solve] khe - Solve 2887

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	11758
+* CURRENT	: 	2887
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -18,42 +18,75 @@ constexpr int INF = 0x3f3f3f3f;
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define pii pair<int,int>
+#define pii pair<int, int>
+#define pip pair<int, pii>
 #define NOP ;
 #define MAXN 100001
 
-int x1, x2, x3, y1, y2, y3;
+int p[MAXN];
+int n, ans = 0;
+vector<pii> vx;
+vector<pii> vy;
+vector<pii> vz;
+vector<pip> mst;
 
-typedef struct coord {
-	int x;
-	int y;
-};
+void init(int x) {
+	for(int i=0; i<x; i++) {
+		p[i] = i;
+	}
+}
 
-int ccw(coord p1, coord p2, coord p3) {
-	return (p2.x-p1.x) * (p3.y-p1.y) - (p3.x-p1.x) * (p2.y - p1.y);
+int find_r(int x) {
+	if(p[x] == x)
+		return x;
+	return p[x] = find_r(p[x]);
+}
+
+void union_r(int x, int y) {
+	x = find_r(x);
+	y = find_r(y);
+
+	if(x != y)
+		p[x] = y;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> x1 >> y1;
-	cin >> x2 >> y2;
-	cin >> x3 >> y3;
+	cin >> n;
+	//init(n);
+	for(int i=1; i<=n; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		vx.push_back({x, i});
+		vy.push_back({y, i});
+		vz.push_back({z, i});
+		p[i] = i;
+	}
+	sort(vx.begin(), vx.end());
+	sort(vy.begin(), vy.end());
+	sort(vz.begin(), vz.end());
 
-	int tmp = ccw({x1, y1}, {x2, y2}, {x3, y3});
-	if(tmp > 0) {
-		cout << 1 << "\n";
-		return 0;
+	for(int i=0; i<n-1; i++) {
+		mst.push_back({abs(vx[i].first - vx[i+1].first), {vx[i].second, vx[i+1].second}});
+		mst.push_back({abs(vy[i].first - vy[i+1].first), {vy[i].second, vy[i+1].second}});
+		mst.push_back({abs(vz[i].first - vz[i+1].first), {vz[i].second, vz[i+1].second}});
+		
 	}
-	if(tmp == 0) {
-		cout << 0 << "\n";
-		return 0;
+	sort(mst.begin(), mst.end());
+
+	for(int i=0; i<mst.size(); i++) {
+		int a = find_r(mst[i].second.first);
+		int b = find_r(mst[i].second.second);
+		
+		if(a != b) {
+			union_r(mst[i].second.first, mst[i].second.second);
+			ans += mst[i].first;
+		}
 	}
-	if(tmp < 0) {
-		cout << -1 << "\n";
-		return 0;
-	}
+	
+	cout << ans << "\n";
 	
 	return 0;
 }
