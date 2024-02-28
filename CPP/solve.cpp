c5cabfe9da144e8ae3a311b16857a14085730f2d
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-02-28 10:22:16 +0000

    [Solve] khe - Solve 1774

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,10 +1,11 @@
 /**************************************************************
-* CURRENT	: 	13418
+* CURRENT	: 	1774
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 #include <memory.h>
 #include <cstring>
 #include <cstdlib>
@@ -22,6 +23,7 @@ typedef unsigned long long ull;
 
 #define pii pair<int, int>
 #define pip pair<int, pii>
+#define pldp pair<long double, pii>
 #define pdd pair<double, double>
 #define pff pair<float, float>
 #define NOP ;
@@ -29,8 +31,9 @@ typedef unsigned long long ull;
 
 int n, m;
 int p[MAXN];
-char prop[MAXN];
-vector<pip> a;
+vector<pldp> a;
+vector<pii> b;
+vector<pii> c;
 
 void init(size_t size) {
 	for(int i=0; i<=size; i++) {
@@ -54,25 +57,23 @@ void union_r(int x, int y) {
 		p[x] = y;
 }
 
+long double getDist(pii p1, pii p2) {
+	return sqrt(pow(p1.first-p2.first, 2) + pow(p1.second-p2.second, 2));
+}
 
-int kruskal() {
-	int c = 0, f = 0;
+long double kruskal() {
+	long double rtn = 0;
 	for(int i=0; i<a.size(); i++) {
-		pip cur = a[i];
+		pldp cur = a[i];
 		int x = cur.second.first;
 		int y = cur.second.second;
 
 		if(find_r(x) == find_r(y))
 			continue;
-		
 		union_r(x, y);
-		if(cur.first == 0)
-			c++;
-		f++;
-		if(f == n)
-			break;
+		rtn += cur.first;
 	}
-	return c*c;
+	return rtn;
 }
 
 
@@ -81,20 +82,42 @@ int main() {
 	cin.tie(0); cout.tie(0);
 
 	cin >> n >> m;
-	for(int i=0; i<m+1; i++) {
-		int u, v, d;
-		cin >> u >> v >> d;	
-		a.push_back({d, {u, v}});
+	init(n);
+	for(int i=0; i<n; i++) {
+		int u, v;
+		cin >> u >> v;	
+		b.push_back({u, v});
+	}
+	for(int i=0; i<m; i++) {
+		int u, v;
+		cin >> u >> v;
+		c.push_back({u, v});
 	}
 
-	init(n);
-	sort(a.begin(), a.end());
-	int ans1 = kruskal();
+	for(int i=0; i<m; i++) {
+		int x = find_r(c[i].first);
+		int y = find_r(c[i].second);
 
-	init(n);
-	sort(a.begin(), a.end(), greater<>());
-	int ans2 = kruskal();
-	
-	cout << ans1 - ans2 << "\n";
+		if(x != y)
+			union_r(x, y);
+	}
+
+	pii p1, p2;
+	for(int i=0; i<n-1; i++) {
+		p1 = b[i];
+		for(int j=i+1; j<n; j++) {
+			p2 = b[j];
+			long double dist = getDist(p1, p2);
+			a.push_back({dist, {i+1, j+1}});
+		}
+	}
+
+	sort(a.begin(), a.end());
+	long double ans = kruskal();
+
+	cout << fixed;
+	cout << setprecision(2);
+	cout << round(ans*100)/100 << endl;
+
 	return 0;
 }
