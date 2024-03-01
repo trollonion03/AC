ee3a8c36676ecc2585c2a7d15c4d8b613afccba1
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-03-01 04:38:44 +0000

    [Solve] khe - Sovle 17490

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	1774
+* CURRENT	: 	17490
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -12,7 +12,7 @@
 #include <cstdio>
 #include <string>
 #include <cmath>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <queue>
 
@@ -27,16 +27,17 @@ typedef unsigned long long ull;
 #define pdd pair<double, double>
 #define pff pair<float, float>
 #define NOP ;
-#define MAXN 1001
+#define MAXN 1000002
 
 int n, m;
+ll k, ans;
+int u, v;
 int p[MAXN];
-vector<pldp> a;
-vector<pii> b;
-vector<pii> c;
+bool cut[MAXN];
+vector<pip> a;
 
 void init(size_t size) {
-	for(int i=0; i<=size; i++) {
+	for(int i=0; i<=size+1; i++) {
 		p[i] = i;
 	}
 }
@@ -47,27 +48,37 @@ int find_r(int x) {
 	return p[x] = find_r(p[x]);
 }
 
+bool isGraph() {
+	for(int i=2; i<=n; i++) {
+		if(find_r(i) != find_r(1)) {
+			return false;
+		}
+	}
+	return true;
+}
+
 void union_r(int x, int y) {
 	x = find_r(x);
 	y = find_r(y);
 
+	if(x == 0 || (x == n && y == 1)) {
+		p[x] = y;
+		return;
+	}
+	
 	if(x < y) 
 		p[y] = x;
 	else
 		p[x] = y;
 }
 
-long double getDist(pii p1, pii p2) {
-	return sqrt(pow(p1.first-p2.first, 2) + pow(p1.second-p2.second, 2));
-}
-
-long double kruskal() {
-	long double rtn = 0;
+ll kruskal() {
+	ll rtn = 0;
 	for(int i=0; i<a.size(); i++) {
-		pldp cur = a[i];
+		pip cur = a[i];
 		int x = cur.second.first;
 		int y = cur.second.second;
-
+		
 		if(find_r(x) == find_r(y))
 			continue;
 		union_r(x, y);
@@ -76,48 +87,61 @@ long double kruskal() {
 	return rtn;
 }
 
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n >> m;
+	cin >> n >> m >> k;
+	if(m <= 1) {
+		cout << "YES\n";
+		return 0;
+	}
 	init(n);
+	for(int i=1; i<=n; i++) {
+		cin >> u;
+		a.push_back({u, {0, i}}); //와우도를 0번 노드로 지정
+	}
+
 	for(int i=0; i<n; i++) {
-		int u, v;
-		cin >> u >> v;	
-		b.push_back({u, v});
-	}
-	for(int i=0; i<m; i++) {
-		int u, v;
 		cin >> u >> v;
-		c.push_back({u, v});
+
+		//강의실이 원형으로 되어있음을 고려
+		if(u == n && v == 1) {
+			cut[u] = true;
+			continue;
+		}
+		else if(u == 1 && v == n) {
+			cut[v] = true;
+			continue;
+		}
+
+		if(u<v)
+			cut[u] = true;
+		else
+			cut[v] = true;
+		
 	}
-
-	for(int i=0; i<m; i++) {
-		int x = find_r(c[i].first);
-		int y = find_r(c[i].second);
-
-		if(x != y)
-			union_r(x, y);
-	}
-
-	pii p1, p2;
-	for(int i=0; i<n-1; i++) {
-		p1 = b[i];
-		for(int j=i+1; j<n; j++) {
-			p2 = b[j];
-			long double dist = getDist(p1, p2);
-			a.push_back({dist, {i+1, j+1}});
+	for(int i=1; i<n; i++) {
+		if(!cut[i]) {
+			union_r(i, i+1);
 		}
 	}
 
-	sort(a.begin(), a.end());
-	long double ans = kruskal();
+	// n번째 노드와 1번째 노드를 연결
+	// 만약에 공사중이면 제외
+	if(!cut[n])
+		union_r(n, 1);
 
-	cout << fixed;
-	cout << setprecision(2);
-	cout << round(ans*100)/100 << endl;
+	if(!isGraph()) {
+		sort(a.begin(), a.end());
+		ans = kruskal();
+		//다리 가설 이후 모든 노드가 연결되었는지 확인
+		if(ans <= k && isGraph()) {
+			cout << "YES\n";
+			return 0;
+		}
+	}
+	cout << "NO\n";
 
 	return 0;
 }
--- PYTHON/solve.py
+++ PYTHON/solve.py
@@ -1,3 +1,8 @@
+li = ['a', 'i', 'e', 'o', 'u']
+cnt = 0
 a = input()
-print(a)
-#hello
+for x in a:
+    if x in li:
+        cnt += 1
+
+print(cnt)
