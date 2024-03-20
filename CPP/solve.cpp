73c555795b488c716a350880359443f2cf94c792
trollonion03 <64517254+trollonion03@users.noreply.github.com>
2024-03-20 09:32:16 +0000

    [Solve] khe - Solve 11726

--- CPP/solve.cpp
+++ CPP/solve.cpp
@@ -1,5 +1,5 @@
 /**************************************************************
-* CURRENT	: 	9095
+* CURRENT	: 	11726
 * NEXT 		: 	NULL
 ***************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
@@ -29,27 +29,24 @@ typedef unsigned long long ull;
 #define pdd pair<double, double>
 #define pff pair<float, float>
 #define NOP ;
-#define MAXN 12
+#define MAXN 10007
 
-int t, n;
+int n;
 int dp[MAXN];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	cin >> t;
+	cin >> n;
 	dp[1] = 1;
 	dp[2] = 2;
-	dp[3] = 4;
-	for(int i=4; i<12; i++) {
-		dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+	for(int i=3; i<=n; i++) {
+		dp[i] = dp[i-1] + dp[i-2];
+		dp[i] %= MAXN;
 	}
 
-	while(t--) {
-		cin >> n;
-		cout << dp[n] << "\n";	
- 	}
+	cout << dp[n] << "\n";
 	
 	return 0;
 }
